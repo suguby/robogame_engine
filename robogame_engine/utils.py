f6cbdf144c62c1a134f8ee4185af78ac5f93b245
wad <suguby@gmail.com>
2016-05-29 14:11:25 +0300

    speed up logging

--- robogame_engine/utils.py
+++ robogame_engine/utils.py
@@ -24,32 +24,32 @@ def _overlapped(left, right):
 class CanLogging(object):
     __logger = None
 
-    @classmethod
-    def get_logger(cls):
-        if cls.__logger is None:
-            logging.config.dictConfig(theme.LOGGING)
-            cls.__logger = logging.getLogger('robogame')
-            if theme.DEBUG:
-                cls.__logger.setLevel('DEBUG')
-            else:
-                cls.__logger.setLevel(theme.LOGLEVEL)
-        return cls.__logger
-
     @property
     def logger(self):
-        return CanLogging.get_logger()
+        if self.__logger is None:
+            logging.config.dictConfig(theme.LOGGING)
+            self.__logger = logging.getLogger('robogame')
+            if theme.DEBUG:
+                self.__logger.setLevel('DEBUG')
+            else:
+                self.__logger.setLevel(theme.LOGLEVEL)
+        return self.__logger
 
     def debug(self, pattern, **kwargs):
-        self._log(self.logger.debug, pattern, kwargs)
+        if self.logger.level == logging.DEBUG:
+            self._log(self.logger.debug, pattern, kwargs)
 
     def info(self, pattern, **kwargs):
-        self._log(self.logger.info, pattern, kwargs)
+        if self.logger.level == logging.INFO:
+            self._log(self.logger.info, pattern, kwargs)
 
     def warning(self, pattern, **kwargs):
-        self._log(self.logger.warning, pattern, kwargs)
+        if self.logger.level == logging.WARNING:
+            self._log(self.logger.warning, pattern, kwargs)
 
     def error(self, pattern, **kwargs):
-        self._log(self.logger.error, pattern, kwargs)
+        if self.logger.level == logging.ERROR:
+            self._log(self.logger.error, pattern, kwargs)
 
     def _log(self, log_fun, pattern, kwargs):
         kwargs['cls'] = self.__class__.__name__
