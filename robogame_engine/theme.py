fd00fe45453429efa4e20713303aff0412f022c2
wad <suguby@gmail.com>
2016-06-02 12:25:15 +0300

    informative exception in theme

--- robogame_engine/theme.py
+++ robogame_engine/theme.py
@@ -23,9 +23,17 @@ class Theme(object):
                 try:
                     value = getattr(constants, item)
                 except AttributeError:
+                    import inspect
+                    stack = inspect.stack()
+                    caller_frame = stack[1]
                     raise AttributeError(
-                        "No constant {} in theme {} and constants".format(
-                            item, self.mod_path
+                        "No constant {theme}.{item}\nFile \"{file}\", line {lineno}, in {func}\n\t{code} ".format(
+                            item=item,
+                            theme=self.mod_path,
+                            file=caller_frame[1],
+                            lineno=caller_frame[2],
+                            func=caller_frame[3],
+                            code=caller_frame[4][0].strip()
                         ))
             self.__dict__[item] = value
         return self.__dict__[item]
