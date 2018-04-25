3a41011ca48207d0828070157a1fef78234c0114
wad <suguby@gmail.com>
2018-04-25 18:48:50 +0300

    python 3.6 to tox, six without versionin setup

--- setup.py
+++ setup.py
@@ -24,8 +24,9 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
-        'six==1.10.0'
+        'six', 'pygame'
     ]
 )
