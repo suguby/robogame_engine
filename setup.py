a7ab7c0bf9fd0607387b8fd8e956104b09ceeeeb
wad <suguby@gmail.com>
2018-09-04 14:08:42 +0300

    0.8.1 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='robogame_engine',
-    version='0.8.1',
+    version='0.8.2',
     packages=['robogame_engine'],
     include_package_data=True,
     license='BSD License',
